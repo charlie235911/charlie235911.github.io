d0df2fa1a09a6d98647677f5d62bf829de992b00
charlie235911 <biau235911@gmail.com>
2023-03-05 18:32:57 +0800

    fix

--- main.js
+++ main.js
@@ -42,7 +42,7 @@ const timerId = setInterval(() => {
             link.style.display = "block";
             setTimeout(function() {
                 window.location.href = 'https://ani.gamer.com.tw/';
-            }, 6000);
+            }, 7000);
         }, 1000);
     }
 }, 1000);
