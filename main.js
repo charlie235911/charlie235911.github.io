6ead85f555b64470af9f099fa180cf3c8b717475
charlie235911 <biau235911@gmail.com>
2023-03-05 16:42:46 +0800

    fix

--- main.js
+++ main.js
@@ -25,7 +25,7 @@ backButton.addEventListener("click", function() {
 
 const progressBar = document.getElementById("progress-bar");
 const progressValue = document.getElementById("progressValue");
-const video = document.getElementById("video");
+const gif = document.getElementById("link-start");
 let value = 0;
 let countdownNumber = 5;
 const timerId = setInterval(() => {
@@ -35,12 +35,9 @@ const timerId = setInterval(() => {
     progressBar.value = value;
     progressValue.innerText = value + "%";
     if (value === 100) {
-        video.style.display = "block";
-        video.play();
-        video.addEventListener("ended", function() {
-            setTimeout(function() {
-                window.location.href = 'https://ani.gamer.com.tw/';
-            });
-        });
+        gif.style.display = "block";
+        setTimeout(function() {
+            window.location.href = 'https://ani.gamer.com.tw/';
+        }, 4900);
     }
 }, 1000);
