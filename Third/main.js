c779ff857d3c2ec045adc60f4d0b5385310ae4e1
charlie235911 <biau235911@gmail.com>
2023-04-23 17:36:05 +0800

    fix

--- Third/main.js
+++ Third/main.js
@@ -1,7 +1,7 @@
 function passwordChangeType(obj) {
-    var passwordInput = document.getElementsByName("password")[0];
-    var nowType = passwordInput.getAttribute("type");
-    if (nowType == "password") {
+    const passwordInput = document.getElementsByName("password")[0];
+    const nowType = passwordInput.getAttribute("type");
+    if (nowType === "password") {
         passwordInput.setAttribute("type", "text");
         obj.getElementsByTagName("i")[0].textContent = "visibility_off";
     } else {
