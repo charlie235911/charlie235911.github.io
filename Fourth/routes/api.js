dd8f83898c58727fe393fb31c5a3309ceb703c11
charlie235911 <biau235911@gmail.com>
2023-05-16 00:21:26 +0800

    add

--- Fourth/routes/api.js
+++ Fourth/routes/api.js
@@ -20,9 +20,9 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/', (req, res) => {
-    const {日期, 商品名稱, 最高價, 最低價}=req.body;
-    sql = "INSERT INTO quote (日期, 商品名稱, 最高價, 最低價) VALUES (?, ?, ?, ?)";
-    db.run(sql, [日期, 商品名稱, 最高價, 最低價], (err) => {
+    const {date, product_name, weight, price}=req.body;
+    sql = "INSERT INTO quote (date, product_name, weight, price) VALUES (?, ?, ?, ?)";
+    db.run(sql, [date, product_name, weight, price], (err) => {
         if (err) {
             console.error(err.message);
             return res.status(500).send(err.message);
